4473085f2a28db438ebbb5bebbf20a82e256ca48
NAVEEN2422008 <70762589+NAVEEN2422008@users.noreply.github.com>
2024-11-30 19:02:57 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -4,12 +4,11 @@ const firebaseConfig = {
   authDomain: "school-6bed0.firebaseapp.com",
   databaseURL: "https://school-6bed0-default-rtdb.firebaseio.com",
   projectId: "school-6bed0",
-  storageBucket: "school-6bed0.appspot.com",
+  storageBucket: "school-6bed0.firebasestorage.app",
   messagingSenderId: "804062217456",
-  appId: "1:804062217456:web:a08c3cf59645607c9dae35",
-  measurementId: "G-X9GYBZKN65"
+  appId: "1:804062217456:web:95ca15cecae868179dae35",
+  measurementId: "G-PJERW5L8ZY"
 };
-  
 if (!firebase.apps.length) {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
